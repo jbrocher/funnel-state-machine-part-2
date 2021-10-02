52b529df06853c1a44bcc2618ea3e28a5976dfb4
jb <jeanb.rocher@gmail.com>
2021-10-02 16:52:45 +0200

    feat(Funnel): Create acquisition funnel

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import FunnelProvider from "./acquisition-funnel/FunnelProvider";
+import StateRenderer from "./acquisition-funnel/StateRenderer";
+import RegistrationStep from "./acquisition-funnel/RegistrationStep";
+import { stateMachine } from "./acquisition-funnel/state-machine";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FunnelProvider stateMachine={stateMachine}>
+        <StateRenderer state="activity">
+          {(send) => {
+            return (
+              <div>
+                <h2>Activity Step</h2>
+                <button onClick={() => send("NEXT")}>next</button>
+              </div>
+            );
+          }}
+        </StateRenderer>
+        <StateRenderer state="register_participant">
+          {(send) => (
+            <RegistrationStep
+              add_participant={() => send("ADD_PARTICIPANT")}
+              additional_information={() => send("ADD_INFO")}
+              proceed_to_payment={() => send("NEXT")}
+            />
+          )}
+        </StateRenderer>
+        <StateRenderer state="additional_information">
+          {(send) => {
+            return (
+              <div>
+                <h2>Additional information</h2>
+                <button onClick={() => send("NEXT")}>next</button>
+              </div>
+            );
+          }}
+        </StateRenderer>
+        <StateRenderer state="payment">
+          {() => {
+            return <h2>payment</h2>;
+          }}
+        </StateRenderer>
+      </FunnelProvider>
     </div>
   );
 }
